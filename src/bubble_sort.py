ba7c510c94cca4515d62a660eb194a59d90b18a7
Attachai-S <attachai.s.work@gmail.com>
2025-03-11 16:06:11 +0700

    update bubble_sort.py

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -7,14 +7,17 @@ def createArr(member): #random member to arrays
 
 arr = []
 createArr(10)
-print(f"original array (before sort): {arr}")
+print(f"original array: {arr} \n")
 
 '''bubble sort algorithm'''
 n = len(arr)
+count = 0 #unnessesarry
 for i in range(n-1):
     for j in range(n-i-1):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
+            count += 1 #unnessesarry
+            print(f"processing round({count}) :{arr}")
 '''bubble sort algorithm'''
 
-print(f"Sorted array (after sort):    {arr}")
+print(f"\nSorted array:    {arr}")
