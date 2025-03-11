89d4db0d826f74ed10ebe6111f24ce8f58030750
Attachai-S <attachai.s.work@gmail.com>
2025-03-11 16:27:16 +0700

    optimize selection_sort .py

--- src/selection_sort.py
+++ src/selection_sort.py
@@ -1,6 +1,8 @@
 import random
 from idlelib.colorizer import prog_group_name_to_tag
 
+from src.bubble_sort import count
+
 
 def createArr(member): #random member to arrays
     for i in range(member):
@@ -11,19 +13,17 @@ arr = []
 createArr(10)
 
 print(f"Original arrays: {arr} \n")
-
+count = 0
 '''selection sort'''
 n = len(arr)
-count = 0 #unnessesarry
-for i in range(n-1):
+for i in range(n):
     min_index = i
     for j in range(i+1, n):
         if arr[j] < arr[min_index]:
             min_index = j
-    min_value = arr.pop(min_index)
-    arr.insert(i, min_value)
-    count += 1 #unnessesarry
-    print(f"processing rount({count}):{arr}")
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+    count += 1
+    print(f"PRocessing (round {count}): {arr}")
 '''selection sort'''
 
 print(f"\nSorted array: {arr}")
